7656c6289c091ca534040677459553fd1a273804
叶煌斌 <yehuangbin@hotmail.com>
2019-06-04 17:50:26 +0800

    dev: 首页icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class='icons'>
+      <swiper>
+          <swiper-slide v-for='(page, index) of pages' :key='index'>
+              <div class="icon" v-for='item of page' :key='item.id'>
+                <img class='icon-img' :src="item.imgUrl" alt="">
+                <p class='icon-p'>{{item.desc}}</p>
+              </div>
+          </swiper-slide>
+      </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data() {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '01'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/b0026eb6f96fd2daecba0d7982c7e4c8.png',
+        desc: '02'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '03'
+      }, {
+        id: '0004',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/bda58ffc3016edad84e656e8a94b0321.png',
+        desc: '04'
+      }, {
+        id: '0005',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/cba147cf6cfcea7109d0bff6aac6f626.png',
+        desc: '05'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/a6/6d97515091789602.png',
+        desc: '06'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '07'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '08'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '09'
+      }
+
+      ]
+    }
+  },
+  computed: {
+    pages() {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+  .icons
+    overflow : hidden
+    height: 0
+    padding-bottom: 50%
+    margin-top: 0
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        margin: 0 auto
+        display: block
+        width: 60%
+        height: 60%
+        left: 0
+        right: 0
+        bottom: .60rem
+      .icon-p
+        position: absolute
+        overflow: hidden
+        white-space: nowrap
+        text-align: center
+        line-height: .44rem
+        height: .44rem
+        bottom: 0
+        left: 0
+        right: 0
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class=wrapper>
+    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+      <swiper-slide v-for='item of swiperList' :key='item.id'>
+        <img class='swiper-img' :src=item.imgUrl>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/2d971885c2a646dd20a55acce76efd35.jpg_750x200_c66112a8.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+/*  >>>表示表头 wrapper下的子组件 */
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: white !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.25%
+    background-color: #eee
+    .swiper-img
+      width: 100%
+</style>
