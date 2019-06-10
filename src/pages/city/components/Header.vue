61332668cdc8773fab1034ed05a845ddab25afd1
叶煌斌 <yehuangbin@hotmail.com>
2019-06-10 15:42:44 +0800

    dev: 城市列表页面

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -1,34 +1,36 @@
-
 <template>
   <div class="header">
-	  <router-link to="/">
-		  <div class="iconfont header-back">&#58916;</div>
-	  </router-link>
-      城市选择
+    <router-link to="/">
+      <div class="header-left">
+        <div class='iconfont back-icon'>&#xe624;</div>
+      </div>
+    </router-link>
+    选择城市
   </div>
 </template>
+
 <script>
 export default {
   name: 'CityHeader'
 }
 </script>
 <style lang="stylus" scoped>
-  @import '~styles/varibles.styl'
+  @import "~styles/varibles.styl"
   .header
-    position: relative
+    position relative
     overflow hidden
     height $headerHeight
     line-height $headerHeight
-    text-align center
     color #ffffff
     background $bgColor
+    text-align center
     font-size .32rem
-	.header-back
-	  position: absolute
-	  top 0
-	  left 0
-	  width: .64rem
-	  text-align: center
-	  font-size: .4rem
-	  color #ffffff
+    .header-left
+      color #ffffff
+      top 0
+      left: 0
+      font-size .4rem
+      width .64rem
+      position absolute
+      text-align center
 </style>
